02fb1707d6e7cf951abf2fdca1095bd2b41c4ac8
Zhongwei Li <zhongweili@tubi.tv>
2023-12-09 19:26:27 +0800

    v4

--- app/main.py
+++ app/main.py
@@ -1,32 +1,78 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from gptool.gptool import Gptool
+import json
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# Mount static files
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
-class Message(BaseModel):
+conversations = {}
+
+gptool = Gptool(
+    openai_key=os.environ.get("OPENAI_KEY"),
+    openai_url=os.environ.get("OPENAI_URL"),
+)
+
+gptool.index()
+print("indexing completed")
+
+
+class SendMessage(BaseModel):
     user_input: str
 
 
+class ClearChat(BaseModel):
+    userId: str
+
+
 @app.get("/", response_class=HTMLResponse)
-async def get_chat(request: Request):
+async def read_root(request: Request):
+    # Render the home page
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.post("/send_message")
-async def send_message(message: Message):
-    user_input = message.user_input
-    if not user_input:
-        raise HTTPException(status_code=400, detail="No message provided")
-    # 这里可以插入处理消息的逻辑，例如调用聊天机器人模型。
-    bot_response = f"Echo: {user_input}"  # Replace this with actual bot response logic
-    return JSONResponse(content={"bot_response": bot_response})
+@app.post("/clear-chat/")
+async def clear_chat(request: ClearChat):
+    print(request)
+    if request.userId in conversations:
+        del conversations[request.userId]
+    return
 
 
-@app.post("/clear_chat")
-async def clear_chat():
-    # This endpoint could be used for backend cleanup if necessary.
-    return JSONResponse(content={"message": "Chat cleared."})
+@app.post("/send-message/")
+async def send_message(request: SendMessage):
+    print(request)
+    user_id = "default_user"
+    message = request.user_input
+
+    if user_id not in conversations:
+        conversations[user_id] = [
+            {
+                "role": "system",
+                "content": """Here are instructions from the user outlining your goals and how you should respond:
+
+I am 'Movies Expert', your go-to assistant for a casual chat with a sprinkle of movie-related humor. Friendly and engaging, I'm here to answer all your film questions based on The Movies Data Base information, provide factual information, offer recommendations, and share personal opinions. Ready to discuss films and dive into the world of cinema with users, I make conversations about movies as entertaining as the films themselves.
+
+If the user starts conversation in non-english language, please respond in the same language.""",
+            }
+        ]
+    conversations[user_id].append({"role": "user", "content": message})
+    messages = conversations[user_id].copy()
+    result = gptool.chat(
+        messages=messages,
+        model="gpt-3.5-turbo-0613",
+        top_n=8,
+    )
+    for message in messages:
+        if "role" not in message:
+            print(message.tool_calls)
+
+    conversations[user_id].append(json.loads(result))
+    return JSONResponse(content={"bot_response": conversations[user_id][-1]["content"]})
